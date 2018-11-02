c99663031c8be8edb92847cf39c9c9c77ddfdce9
yBernier <webyves@hotmail.com>
2018-11-02 12:44:31 +0100

    comment main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,11 @@ Main JS file for custom JS
 
 $(document).ready(function() {
 
+    /****************************************
+        Function for response comments
+        put id in hidden input in modal
+    ****************************************/
     $('.btn_action_reply').on('click', function(){
-        // alert($(this).data("idcom"));
         $('#respComInputIdCom').val($(this).data("idcom"));
         $('#respComModal').modal('show');
     });
